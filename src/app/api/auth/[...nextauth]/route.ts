7e857bbd9b98d6baf3edcaa0b5e3e083a143cf3b
rockomatthews <rob@robmatthe.ws>
2025-04-24 10:41:56 -0600

    adding vercel3

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "../../../../lib/auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }; 
+export const GET = handler;
+export const POST = handler; 
